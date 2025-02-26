20a7d780dceb7e9b99b136d63bc98a6c5cc0cfb0
lepaulinux <63528751+lepaulinux@users.noreply.github.com>
2025-02-25 22:05:05 -0500

    day6
    Maze - day6

---
+++ Maze.py
@@ -0,0 +1,37 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+
+while front_is_clear():
+    move()
+while not at_goal():
+    if wall_on_right() and front_is_clear():
+        move()
+    elif not wall_on_right():
+        turn_right()
+    elif wall_on_right() and wall_in_front:
+        turn_left()
+    elif wall_on_right() and wall_in_front():
+        turn_left()
+    elif not wall_on_right():
+        turn_right()
+    elif not wall_on_right() and wall_in_front:
+        turn_left()
+
+
+
+    
+        
+
+  
+    
+
+
+ 
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
